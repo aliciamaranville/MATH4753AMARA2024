1e7ab8f54b6a9bad2b137db689ccdc3b9aa7f7f4
Alicia Maranville <alicia.d.maranville-1@ou.edu>
2024-10-08 21:26:39 -0500

    project 1

--- R/ntickets.R
+++ R/ntickets.R
@@ -1,15 +1,44 @@
+#' Project 1 Overbooking Problem
+#'
+#' @param N number of seats in the flight
+#' @param gamma probability of plane being overbooked
+#' @param p probability of a 'show'
+#' @importFrom graphics abline layout
+#' @importFrom stats pbinom uniroot
+#'
+#' @return named list with discrete and continuous and 2 plots
+#' @export
+#'
+#' @examples ntickets(N=200, gamma=0.02, p=0.95)
 ntickets = function(N, gamma, p) {
-  # discrete calculation
+  x=NULL
+  # discrete calculation: use binomial
+  fd <- function(n) 1-gamma-pbinom(q=N, size=n, prob=p)
+  for (n in N:(N*1.1)) {
+    if (fd(n) >= 0) {
+      nd <- n # breaks at first positive value, closest to zero
+      break
+    }
+  }
 
-
-  # continuous calculation
-
+  # continuous calculation: use normal
+  fc <- function(x) 1-gamma-pnorm(q=N+0.5, mean=x*p, sd=sqrt(x*p*(1-p)))
+  nc <- uniroot(fc, interval=c(N,N*1.10))[[1]]
 
   # named list
-
+  print(list(nd=nd, nc=nc, N=N, p=p, gamma=gamma))
 
   # discrete plot
 
+  layout(matrix(1:2, nrow=2, ncol=1))
+
+  x <- seq(N,N*1.1, by=1)
+  plot(x, y=fd(x), type = "b", pch = 16, col = "navy", main = "Objective Vs n to find optimal tickets sold\n (205) gamma=0.02 N=200 discrete", xlab = "n", ylab = "Objective")
+  abline(h = 0, col = "red", lwd = 2)
+  abline(v = nd, col = "red", lwd = 2)
 
   # continuous plot
+  curve(1-gamma-pnorm(q=N+0.5, mean=x*p, sd=sqrt(x*p*(1-p))), xlim=c(N,N*1.1), main="Objective Vs n to find optimal tickets sold\n (204.3178) gamma=0.02 N=200 continuous", xlab = "n", ylab = "Objective")
+  abline(h = 0)
+  abline(v = nc)
 }
